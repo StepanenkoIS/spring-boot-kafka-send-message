f189eb3c7a5f5eab42465e81718b502c924019dc
StepanenkoIS <Citizen(18)>
2021-06-07 14:27:02 +0400

    gitignore

---
+++ src/main/java/is/springbootkafkasendmessage/SpringBootKafkaSendMessageApplication.java
@@ -0,0 +1,14 @@
+package is.springbootkafkasendmessage;
+
+        import org.springframework.boot.SpringApplication;
+        import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringBootKafkaSendMessageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootKafkaSendMessageApplication.class, args);
+    }
+
+}
